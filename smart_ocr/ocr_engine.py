66fddfa1f4dcc355d7185bdfc0e9432574ea30a0
Krithik <krithiksehgal@gmail.com>
2025-05-08 19:53:19 -0700

    Add translation for hindi and change theme

--- smart_ocr/ocr_engine.py
+++ smart_ocr/ocr_engine.py
@@ -12,5 +12,5 @@ def extract_text(bbox):
         monitor = {"top": y1, "left": x1, "width": width, "height": height}
         sct_img = sct.grab(monitor)
         img = Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX")
-        text = pytesseract.image_to_string(img)
+        text = pytesseract.image_to_string(img, lang='eng+hin')
         return text.strip()
